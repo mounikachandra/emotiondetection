289d82d4c07770caeb81414b0dd543892a84af1f
Gandavarapu Mounika <mgandava@email.asu.edu>
2018-09-20 20:39:32 -0700

    Tested images to validate the output percentage

---
+++ emotiondetection/morph_four_neigh.m
@@ -0,0 +1,20 @@
+%author : mounika
+
+function [input_img] = morph_four_neigh(input_img)
+%UNTITLED Summary of this function goes here
+%   Detailed explanation goes here
+%Morphology on the image
+
+
+for a= 2:size(input_img,1)-1
+    for b= 2:size(input_img,2)-1
+        if(input_img(a,b)>1)
+            if(input_img(a,b-1)+input_img(a,b+1)+input_img(a-1,b)+input_img(a+1,b)==0)
+                input_img(a,b)=0;
+            end
+        end
+    end
+end
+
+end
+
---
+++ emotiondetection/plot_code.m
@@ -0,0 +1,25 @@
+%Function of ploting plots on the image
+function[]=plot_code(p1x,p1y,p2x,p2y,p3x,p3y,p4x,p4y,p5x,p5y,p6x,p6y,p7x,p7y,p8x,p8y,path)
+66666666666666666666666666666666666666666
+plot_img=imread(path);
+plot_img_mod=rgb2gray(plot_img);
+imshow(plot_img_mod);hold on
+plot(p1x,p1y,'r.-'),hold on
+plot(p2x,p2y,'r.-'),hold on
+plot(p3x,p3y,'r.-'),hold on
+plot(p4x,p4y,'r.-'),hold on
+plot(p5x,p5y,'r.-'),hold on
+plot(p6x,p6y,'r.-'),hold on
+plot(p7x,p7y,'r.-'),hold on
+plot(p8x,p8y,'r.-'),hold on
+
+end
+% [I, map] =  imread('G:\fyproject\jaffeimages\jaffecrop\13.tiff');
+% axis image;
+% image (I);
+% colormap(map);
+% hold on;
+% x1 =16,
+% y1 = 1;
+% figure(2);
+% plot(x1,y1,'r.-');
